3591e332d0b227c6c7b18869752f9e9d19d45711
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-18 10:27:57 -0400

    Created the generate password function to contain all previously written code.

--- script.js
+++ script.js
@@ -13,23 +13,43 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Generate password
+function generatePassword() {
+
+// Continues to prompt user to input a number until it falls between the min./max. requirements.
 var pwLength = parseInt(prompt("Enter a number between 8-128"));
 while (pwLength < 7 || pwLength > 129 || isNaN(pwLength) || pwLength === null) {
   alert("Please enter a valid number between 8-128.");
   pwLength = prompt("Enter a number between 8-128");
 }
 
-// Define variables that allow the user to determine password character types.
+console.log(pwLength);
+
+// Defines variables that allow the user to determine password character types.
 var confirmLower = confirm("Do you want to include lower case characters?");
 var confirmUpper = confirm("Do you want to include upper case characters?");
 var confirmNumber = confirm("Do you want to include number characters?");
 var confirmSpecial = confirm("Do you want to include special characters?");
 
-console.log(pwLength);
+// Continues to prompt user to enter valid character type choice until criteria met.
+while (confirmLower === false && confirmUpper === false && confirmNumber === false && confirmSpecial === false) {
+  alert ("Please select at least one character type.");
+  confirmLower = confirm("Do you want to include lower case characters?");
+  confirmUpper = confirm("Do you want to include upper case characters?");
+  confirmNumber = confirm("Do you want to include number characters?");
+  confirmSpecial = confirm("Do you want to include special characters?");
+}
 
+console.log(confirmLower);
+console.log(confirmUpper);
+console.log(confirmNumber);
+console.log(confirmSpecial);
+
+// Defines arrays of different character types.
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "Y", "V", "W", "X", "Y", "Z"];
 var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 
+}
