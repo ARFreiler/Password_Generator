ffcf4feef48513248808b48dedc3ab38f60fc073
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-17 22:56:03 -0400

    Added propmt to capture user password length and ensure it meets min/max password requirements. Also created variables to confirm user character choices for password.

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var pwLength = parseInt(prompt("Enter a number between 8-128"));
+  while (pwLength < 7 || pwLength > 129 || isNaN(pwLength) || pwLength === null) {
+      alert("Please enter a valid number between 8-128.");
+      pwLength = prompt("Enter a number between 8-128");
+}
+
+// Define variables that allow the user to determine password character types.
+var confirmLower = confirm("Do you want to include lower case characters?");
+var confirmUpper = confirm("Do you want to include upper case characters?");
+var confirmNumber = confirm("Do you want to include number characters?");
+var confirmSpecial = confirm("Do you want to include special characters?");
+
+console.log(pwLength);
+
+ 
+
