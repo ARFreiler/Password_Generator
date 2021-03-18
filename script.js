f32c0af6b4a5f73ad7edaac04f83bc30183fe823
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-18 09:14:52 -0400

    Created arrays containing strings for loer, upper, number, and special characters.

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 var pwLength = parseInt(prompt("Enter a number between 8-128"));
-  while (pwLength < 7 || pwLength > 129 || isNaN(pwLength) || pwLength === null) {
-      alert("Please enter a valid number between 8-128.");
-      pwLength = prompt("Enter a number between 8-128");
+while (pwLength < 7 || pwLength > 129 || isNaN(pwLength) || pwLength === null) {
+  alert("Please enter a valid number between 8-128.");
+  pwLength = prompt("Enter a number between 8-128");
 }
 
 // Define variables that allow the user to determine password character types.
@@ -27,5 +27,9 @@ var confirmSpecial = confirm("Do you want to include special characters?");
 
 console.log(pwLength);
 
- 
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "Y", "V", "W", "X", "Y", "Z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+
 
