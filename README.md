0f8dfdab836c2ef1804089b26d89e40ac36364a0
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-03-21 09:09:24 -0400

    Added all content of the README file except the final repo and deployed page links and deployed page screenshot.

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Password_Generator
+
+## Table of Contents
+
+[Description](#description)
+
+[Installation](#installation)
+
+[Credits](#credits)
+
+[Links](#links)
+
+[License](#license)
+
+## Description
+
+The purpose of this project was to create a means by which employees working with sensitive materials could better protect their work by creating random password's as an added security measure. The password 
+generator allows the user to input their choice of desired keyboard characters and password length and will return a random password based upon the user's input.
+
+## Installation
+
+Open index.html in Chrome.
+
+## Credits
+
+[W3Schools](https://www.w3schools.com)
+
+[Tutorial Republic](https://www.tutorialrepublic.com)
+
+[FreeCodeCamp](https://www.freecodecamp.org/)
+
+[MDN Documents](https://developer.mozilla.org/en-US/docs/Learn)
+
+[JavaScript Tutorial](https://javascripttutorial.net/)
+
+## Links
+
+[Repository]()
+
+[Password Generator]()
+
+
+
+## License
+
+The MIT License (MIT) Copyright © 2021 Ashleigh Freiler
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -16,40 +16,75 @@ generateBtn.addEventListener("click", writePassword);
 // Generate password
 function generatePassword() {
 
-// Continues to prompt user to input a number until it falls between the min./max. requirements.
-var pwLength = parseInt(prompt("Enter a number between 8-128"));
-while (pwLength < 7 || pwLength > 129 || isNaN(pwLength) || pwLength === null) {
-  alert("Please enter a valid number between 8-128.");
-  pwLength = prompt("Enter a number between 8-128");
-}
+  // Continues to prompt user to input a number until it falls between the min./max. requirements.
+  var pwLength = parseInt(prompt("Enter a number between 8-128"));
+  while (pwLength < 7 || pwLength > 129 || isNaN(pwLength) || pwLength === null) {
+    alert("Please enter a valid number between 8-128.");
+    pwLength = prompt("Enter a number between 8-128");
+  }
 
-console.log(pwLength);
+  console.log(pwLength);
 
-// Defines variables that allow the user to determine password character types.
-var confirmLower = confirm("Do you want to include lower case characters?");
-var confirmUpper = confirm("Do you want to include upper case characters?");
-var confirmNumber = confirm("Do you want to include number characters?");
-var confirmSpecial = confirm("Do you want to include special characters?");
+  // Defines variables that allow the user to determine password character types.
+  var confirmLower = confirm("Do you want to include lower case characters?");
+  var confirmUpper = confirm("Do you want to include upper case characters?");
+  var confirmNumber = confirm("Do you want to include number characters?");
+  var confirmSpecial = confirm("Do you want to include special characters?");
 
-// Continues to prompt user to enter valid character type choice until criteria met.
-while (confirmLower === false && confirmUpper === false && confirmNumber === false && confirmSpecial === false) {
-  alert ("Please select at least one character type.");
-  confirmLower = confirm("Do you want to include lower case characters?");
-  confirmUpper = confirm("Do you want to include upper case characters?");
-  confirmNumber = confirm("Do you want to include number characters?");
-  confirmSpecial = confirm("Do you want to include special characters?");
-}
+  // Continues to prompt user to enter valid character type choice until criteria met.
+  while (confirmLower === false && confirmUpper === false && confirmNumber === false && confirmSpecial === false) {
+    alert("Please select at least one character type.");
+    confirmLower = confirm("Do you want to include lower case characters?");
+    confirmUpper = confirm("Do you want to include upper case characters?");
+    confirmNumber = confirm("Do you want to include number characters?");
+    confirmSpecial = confirm("Do you want to include special characters?");
+  }
 
-console.log(confirmLower);
-console.log(confirmUpper);
-console.log(confirmNumber);
-console.log(confirmSpecial);
+  console.log(confirmLower);
+  console.log(confirmUpper);
+  console.log(confirmNumber);
+  console.log(confirmSpecial);
 
-// Defines arrays of different character types.
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "Y", "V", "W", "X", "Y", "Z"];
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  // Defines arrays of different character types.
+  var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "Y", "V", "W", "X", "Y", "Z"];
+  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
+  // Array of user approved characters.
+  var passPool = [];
+
+  // Array generated based upon user's responses.
+  function generateChar() {
+    if (confirmLower) {
+      passPool.push(...lower);
+    }
+    if (confirmUpper) {
+      passPool.push(...upper);
+    }
+    if (confirmNumber) {
+      passPool.push(...number);
+    }
+    if (confirmSpecial) {
+      passPool.push(...special);
+    }
+  }
+
+  generateChar();
+  console.log(passPool);
+
+  // var passIndex = Math.floor(Math.random() * passPool.length);
+  // var randomPass = passPool[passIndex];
+
+  function pushChar() {
+    var randomPassword = [];
+    for (let i = 0; i < pwLength; i++) {
+      var index = Math.floor(Math.random() * passPool.length);
+      console.log(passPool[index]);
+      randomPassword.push(item);
+
+    }
+
+  }
 
 }
